db2d677d0cea188b24104c84f9ac1d3fe5cdf1ed
Tonii18 <ajsanrosal@gmail.com>
2026-02-01 20:20:57 +0100

    Undo changes due to not working

--- src/main/java/com/example/demo/security/JwtAuthenticationFilter.java
+++ src/main/java/com/example/demo/security/JwtAuthenticationFilter.java
@@ -1,11 +1,12 @@
 package com.example.demo.security;
 
 import java.io.IOException;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
@@ -25,60 +26,27 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 	@Autowired
 	private CustomUserDetailsService customUserDetailsService;
 
-//	@Override
-//	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
-//			throws ServletException, IOException {
-//
-//		String jwt = getJwtFromRequest(request);
-//
-//		if (StringUtils.hasText(jwt) && tokenProvider.validateToken(jwt)) {
-//
-//			String username = tokenProvider.getEmailFromJWT(jwt);
-//			String role = tokenProvider.getRoleFromJWT(jwt); // ROLE_ADMIN
-//
-//			UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(username, null,
-//					List.of(new SimpleGrantedAuthority(role)));
-//
-//			authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
-//
-//			SecurityContextHolder.getContext().setAuthentication(authentication);
-//		}
-//
-//		filterChain.doFilter(request, response);
-//	}
-	
 	@Override
-	protected void doFilterInternal(HttpServletRequest request,
-	                                HttpServletResponse response,
-	                                FilterChain filterChain)
-	        throws ServletException, IOException {
+	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+			throws ServletException, IOException {
 
-	    String jwt = getJwtFromRequest(request);
+		String jwt = getJwtFromRequest(request);
 
-	    if (StringUtils.hasText(jwt) && tokenProvider.validateToken(jwt)) {
+		if (StringUtils.hasText(jwt) && tokenProvider.validateToken(jwt)) {
 
-	        String email = tokenProvider.getEmailFromJWT(jwt);
+			String username = tokenProvider.getEmailFromJWT(jwt);
+			String role = tokenProvider.getRoleFromJWT(jwt); // ROLE_ADMIN
 
-	        UserDetails userDetails =
-	                customUserDetailsService.loadUserByUsername(email);
+			UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(username, null,
+					List.of(new SimpleGrantedAuthority(role)));
 
-	        UsernamePasswordAuthenticationToken authentication =
-	                new UsernamePasswordAuthenticationToken(
-	                        userDetails,
-	                        null,
-	                        userDetails.getAuthorities()
-	                );
+			authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
 
-	        authentication.setDetails(
-	                new WebAuthenticationDetailsSource().buildDetails(request)
-	        );
+			SecurityContextHolder.getContext().setAuthentication(authentication);
+		}
 
-	        SecurityContextHolder.getContext().setAuthentication(authentication);
-	    }
-
-	    filterChain.doFilter(request, response);
+		filterChain.doFilter(request, response);
 	}
-
 
 	private String getJwtFromRequest(HttpServletRequest request) {
 		String bearerToken = request.getHeader("Authorization");
